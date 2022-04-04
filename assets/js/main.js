1bfefcf417b6f5a72e020a836913defd9903ba38
Dax2976 <ferrodavide55@gmail.com>
2022-04-04 16:22:19 +0200

    add function colorcell in hard

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,60 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+
+
+// creazioni variabili 
+
+let btn = document.getElementById('btn')
+let grid = document.getElementById('grid')
+let selezione = document.getElementById('selezione')
+// let btnReset = document.getElementById('btnReset')
+
+
+// al click si refersha la pagina
+btnReset.onclick = reload
+
+
+
+// al click crea le celle in base alla difficoltà
+btn.onclick = createCell
+
+
+
+// funzioni
+
+function reload (){
+   location.reload()
+}
+
+
+function createCell(){
+    if(selezione.value == 'dif1'){
+        for(let i = 1; i <= 100; i++){
+            let content = document.createElement('div')
+            content.classList.add('diff_1')
+            content.innerHTML = i
+            grid.appendChild(content)
+        }
+    } else if(selezione.value == 'dif2'){
+        for(let i = 1; i <= 81  ;i++){
+            let content = document.createElement('div')
+            content.classList.add('diff_2')
+            content.innerHTML = i
+            grid.appendChild(content)
+        }
+    } else if(selezione.value == 'dif3'){
+        for(let i = 1; i <= 49  ;i++){
+            let content = document.createElement('div')
+            content.classList.add('diff_3')
+            content.innerHTML = i
+            grid.appendChild(content)
+            content.onclick = colorCell
+        }
+    }
+}
+
+function colorCell(){
+    this.classList.add('bgbox')
+}
